46d7726f88de4e304acbb671d26098529a465afd
Evgeniy Nikulov <3736932+NikulovE@users.noreply.github.com>
2024-05-13 17:40:53 +0300

    Update delete.sh

--- api-scripts/delete.sh
+++ api-scripts/delete.sh
@@ -2,6 +2,6 @@
 data=$(echo -n '{"accessKey":"'"$MINIO_USER_WRITER"'","secretKey":"'"$MINIO_USER_WRITER_PASSWORD"'"}')
 curl --location "https://${MINIO_HOST}.${DOMAIN_FQDN}/api/v1/login" --header 'Content-Type: application/json' --cookie-jar cookies.txt --data "$data"
 
-targetPath="iaas"
+targetPath="iaas/"
 encodedTargetPath=$(echo -n ${targetPath}| base64 -w 0)
 curl -X DELETE "https://${MINIO_HOST}.${DOMAIN_FQDN}/api/v1/buckets/${backet}/objects?prefix=$encodedTargetPath&all_versions=false&bypass=false&recursive=true" --cookie cookies.txt
